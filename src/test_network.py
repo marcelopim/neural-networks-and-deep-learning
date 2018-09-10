81763ad58c633ae7c637c03a5fa8d366c12a043b
Marcelo Pimentel <marcelo.ide.pimentel@gmail.com>
2018-09-10 01:49:04 -0300

    added a script for testing the neural network

---
+++ src/test_network.py
@@ -0,0 +1,14 @@
+import mnist_loader
+# import network2_mod as network
+# import network2 as network
+import network_mod_py36 as network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+
+net.SGD_niin(training_data, 30, 10, 10.0, lmbda = 100.0, evaluation_data=validation_data, monitor_evaluation_accuracy=True)
+# net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data, monitor_evaluation_accuracy=True)
+
+# net.save('pesos')
